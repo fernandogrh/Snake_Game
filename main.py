5bc18c0305a5f0fb11e846aaa532c44af70cc6e3
fernandogrh <gh05t290805@gmail.com>
2025-12-02 20:13:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game Project FR")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+play = True
+while play:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with Food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # detect collision with Wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        play = False
+        scoreboard.game_over()
+
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 15:
+            play = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
+up, down, left, right = 90, 270, 180, 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in starting_positions:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        for segment_number in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[segment_number - 1].xcor()
+            y = self.segments[segment_number - 1].ycor()
+            self.segments[segment_number].goto(x, y)
+        self.head.forward(move_distance)
+
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+
+    def left(self):
+        if self.head.heading() != right:
+            self.head.setheading(left)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
